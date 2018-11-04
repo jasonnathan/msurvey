abff479509c4bfae8af0f8479c845031863f40f2
Jason Nathan <jnathan@palo-it.com>
2018-11-05 03:05:12 +0800

    Updated other account forms to use new components

--- imports/ui/features/Accounts/components/Login.js
+++ imports/ui/features/Accounts/components/Login.js
@@ -7,6 +7,8 @@
 import React from 'react';
 import { Link } from '@react-navigation/web';
 
+import Form from '/imports/ui/components/Form';
+import FloatingLabelInput from '/imports/ui/components/FloatingLabelInput';
 import CenteredCircularLoading from '/imports/ui/components/CenteredCircularLoading';
 
 export default class Login extends React.PureComponent {
@@ -44,29 +46,27 @@ export default class Login extends React.PureComponent {
       <div className="col s12 m8 l6 offset-m2 offset-l3">
         <h4 className="light-blue-text text-lighten-1">{title}</h4>
         {this.showError()}
-        <form className="z-depth-1 bordered slim" method="post" id="loginForm">
+        <Form className="z-depth-1 bordered slim" method="post" id="loginForm">
           <br />
-          <div className="input-field suffix col s12" id="email">
-            <input type="email" name="email" className="validate" required="" />
-            <label
-              className="col s12"
-              htmlFor="email"
-              data-error="Please enter a valid email address"
-            >
-              Email Address
-            </label>
-          </div>
-          <div className="input-field suffix col s12" id="password">
-            <input
-              name="password"
-              type="password"
-              className="validate"
-              required
-            />
-            <label className="col s12" htmlFor="password">
-              Password
-            </label>
-          </div>
+          <FloatingLabelInput
+            required
+            label="Email Address"
+            labelClassName="col s12"
+            name="username"
+            type="email"
+            errorMessage="Please enter a valid email address"
+            containerClassName="input-field suffix col s12"
+          />
+          <FloatingLabelInput
+            required
+            label="Password"
+            labelClassName="col s12"
+            name="password"
+            type="password"
+            pattern=".{8,}"
+            errorMessage="Your password should be at least 8 characters long"
+            containerClassName="input-field suffix col s12"
+          />
           <button
             type="submit"
             className="waves-effect waves-light btn btn-large col s12 light-blue accent-3"
@@ -74,7 +74,7 @@ export default class Login extends React.PureComponent {
             Sign In
           </button>
           <Link routeName="ForgotPassword">Forgot your password?</Link>
-        </form>
+        </Form>
       </div>
     );
   }
--- imports/ui/features/Accounts/components/ResetPassword.js
+++ imports/ui/features/Accounts/components/ResetPassword.js
@@ -6,6 +6,9 @@
  */
 import React from 'react';
 
+import Form from '/imports/ui/components/Form';
+import FloatingLabelInput from '/imports/ui/components/FloatingLabelInput';
+
 export default class ResetPassword extends React.PureComponent {
   static defaultProps = {
     errorMessage: null
@@ -25,44 +28,31 @@ export default class ResetPassword extends React.PureComponent {
       <div className="col s12 m8 l6 offset-m2 offset-l3">
         <h4 className="light-blue-text text-lighten-1">Reset your password</h4>
         {this.showError()}
-        <form
+        <Form
           className="z-depth-1 bordered slim"
           method="post"
           id="resetpasswordForm"
         >
           <br />
-          <div className="input-field suffix col s12" id="resetPassword">
-            <input
-              type="password"
-              className="validate"
-              name="resetPassword"
-              required
-              pattern=".{8,}"
-            />
-            <label
-              htmlFor="resetPassword"
-              className="col s12"
-              data-error="Your password should be at least 8 characters long"
-            >
-              New Password
-            </label>
-          </div>
-          <div className="input-field suffix col s12" id="confirmResetPassword">
-            <input
-              type="password"
-              className="validate"
-              name="confirmResetPassword"
-              required
-            />
-            >
-            <label
-              htmlFor="confirmResetPassword"
-              className="col s12"
-              data-error="Your passwords do not match"
-            >
-              Confirm Password
-            </label>
-          </div>
+          <FloatingLabelInput
+            required
+            label="New Password"
+            labelClassName="col s12"
+            name="resetPassword"
+            type="password"
+            pattern=".{8,}"
+            errorMessage="Your password should be at least 8 characters long"
+            containerClassName="input-field suffix col s12"
+          />
+          <FloatingLabelInput
+            required
+            label="Confirm Password"
+            labelClassName="col s12"
+            name="confirmResetPassword"
+            type="password"
+            errorMessage="Your passwords do not match"
+            containerClassName="input-field suffix col s12"
+          />
           <button
             type="submit"
             className="waves-effect waves-light btn btn-large col s10 offset-s1 light-blue accent-3"
@@ -70,7 +60,7 @@ export default class ResetPassword extends React.PureComponent {
           >
             Submit
           </button>
-        </form>
+        </Form>
       </div>
     );
   }
