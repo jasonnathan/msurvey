7de659e7123d51f248340c42e1610be2d821e918
Jason Nathan <jnathan@palo-it.com>
2018-11-05 09:49:24 +0800

    Added redux

--- client/main.js
+++ client/main.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
- 
+import { Provider } from 'react-redux';
+
 import App from '../imports/ui/App.js';
- 
+import store from '../imports/ui/store';
+
 Meteor.startup(() => {
-  render(<App />, document.getElementById('render-target'));
+  render(
+    <Provider store={ store }>
+      <App />
+    </Provider>,
+    document.getElementById('render-target')
+  );
 });
---
+++ imports/ui/scenes/rootReducer.js
@@ -0,0 +1,4 @@
+import { combineReducers } from 'redux';
+import { reducer as formReducer } from '/imports/ui/features/Form';
+
+export default combineReducers({ formReducer });
